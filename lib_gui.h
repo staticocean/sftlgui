95f565d3886869707593866137b65cd5cbb39746
softael <softael@outlook.com>
2021-05-19 03:07:02 +0300

    [F] [proj] all libs are added with cmake

---
+++ lib_gui.h
@@ -0,0 +1,14 @@
+
+#ifndef __LIBGUI__
+#define __LIBGUI__
+
+//----------------------------------------------------------------
+
+#include <res/fonts/default_font.h>
+#include <lib/imgui/imgui.h>
+#include <lib/implot/implot.h>
+#include <nfd.h>
+
+//----------------------------------------------------------------
+
+#endif /* __LIBGUI__ */
